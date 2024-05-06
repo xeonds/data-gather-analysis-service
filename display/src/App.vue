5ed96e56ece6d02c77a3036bef98ac7a90ab2887
xeonds <xeonds@stu.xidian.edu.cn>
2024-05-06 21:27:22 +0800

    feat: completed display

--- display/src/App.vue
+++ display/src/App.vue
@@ -1,14 +1,16 @@
 <template>
     <div>
         <div>
-            <label for="device-select">选择设备：</label>
-            <select id="device-select" v-model="selectedDevice" @change="switchDisplayDevice">
-                <option v-for="device in deviceList" :key="device.id" :value="device.id">{{ device.name }}</option>
-            </select>
+            <label>选择设备：</label>
+            <span v-for="device in deviceList" :key="device.id">
+                <input type="radio" :id="'device-radio-' + device.id" :value="device.id" v-model="selectedDevice">
+                <label :for="'device-radio-' + device.id">{{ device.name }}</label>
+            </span>
         </div>
         <div>
             <div id="chart" style="width: 600px; height: 400px;"></div>
         </div>
+        <p>{{ data[selectedDevice] }}</p>
     </div>
 </template>
 
@@ -24,34 +26,13 @@ const ws: Ref<WebSocket> = ref({} as WebSocket)
 const url = ref('ws://localhost:8001/ws')
 const data: Ref<any> = ref({})
 
-const fetchDeviceList = () => {
+const fetchDeviceList = () =>
     fetch('/count')
         .then(response => response.json())
         .then(data => deviceList.value = Array.from({ length: data.count }, (_, i) => ({ id: i, name: `设备${i}` })))
         .catch(error => console.error('Error fetching device list:', error));
-};
 
-const switchDisplayDevice = () => {
-    analysisData.value = data.value[selectedDevice.value] || {};
-};
-
-watch(selectedDevice, () => switchDisplayDevice());
-watch(analysisData, () => {
-    if (analysisData.value) {
-        chart.value.setOption({
-            title: { text: '数据分析图表' },
-            xAxis: {
-                type: 'category',
-                data: ['Max', 'Min', 'Avg', 'Variance']
-            },
-            yAxis: { type: 'value' },
-            series: [{
-                data: [analysisData.value.Max, analysisData.value.Min, analysisData.value.Avg, analysisData.value.Variance],
-                type: 'bar'
-            }]
-        });
-    }
-}, { deep: true });
+const switchDisplayDevice = () => analysisData.value = data.value[selectedDevice.value];
 
 const initWebSocket = (addr: string, callback: Function) => {
     const socket = new WebSocket(addr)
@@ -72,10 +53,28 @@ const conn = () => {
         url.value,
         (e: string) => {
             const parsed = JSON.parse(e)
-            data.value[parsed.id] = parsed.data
-            switchDisplayDevice()
+            data.value[parsed.Device] = parsed
+            analysisData.value = data.value[selectedDevice.value]
         })
 }
+
+watch(selectedDevice, () => switchDisplayDevice());
+watch(analysisData, () => {
+    if (analysisData.value) {
+        chart.value.setOption({
+            title: { text: '数据分析图表' },
+            xAxis: {
+                type: 'category',
+                data: ['Max', 'Min', 'Avg', 'Variance']
+            },
+            yAxis: { type: 'value' },
+            series: [{
+                data: [analysisData.value.Max, analysisData.value.Min, analysisData.value.Avg, analysisData.value.Variance],
+                type: 'bar'
+            }]
+        });
+    }
+}, { deep: true });
 
 onMounted(() => {
     fetchDeviceList();
